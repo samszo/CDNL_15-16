adf447a8b39e102caaed10ac009ab3116b5913d2
CrazyYoshi <miloud.maamar@gmail.com>
2015-10-12 00:54:36 +0200

    Conflit SSL empêchant la récupération de données.
    Par défaut cURL ne fait confiance à aucune certification
    
    Avec la ligne ajoutée, on l'oblige à faire confiance à n'importe qui entre autre.
    Pour une communication de données plus sensible, cette méthode est à éviter.
    
    Source  : http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/

--- php/lecteurFlux.php
+++ php/lecteurFlux.php
@@ -9,8 +9,14 @@ function curl($url){
 	$handle = curl_init();
 	curl_setopt($handle, CURLOPT_URL, $url);
     curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+ // Solution temporaire - Soucis conflit ssl-https
+ // source : http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/
+    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
     $response = curl_exec($handle);
     curl_close($handle);
     
     echo $response;
+
 }
