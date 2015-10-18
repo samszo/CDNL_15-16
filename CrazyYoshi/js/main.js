5c11c728c9b8e1bca328edf3dac5d9e87ca7d5e4
CrazyYoshi <miloud.maamar@gmail.com>
2015-10-18 19:37:50 +0200

    Traitement de l'information et début d'une mise en forme de celle ci

--- CrazyYoshi/js/main.js
+++ CrazyYoshi/js/main.js
@@ -1,10 +1,13 @@
 var dataEtu;
 var xmlDoc;
 var photos = new Array();
+var html = "";
 
 $(document).ready(
     function () {
-        d3.csv("../php/lecteurFlux.php?url=cdnl1516data", function (data){xmlDoc=data;});
+        d3.csv("../php/lecteurFlux.php?url=cdnl1516data", function (data) {
+            xmlDoc = data;
+        });
         $.ajax({
             type: "GET",
             url: "../php/lecteurFlux.php?url=cdnl1516photo",
@@ -12,39 +15,50 @@ $(document).ready(
             success: function (xml) {
                 return xml;
             }
-        }).done(function(data){
+        }).done(function (data) {
             var i = 0;
             $(data).find('enclosure').each(
                 function () {
                     photos[i] = new Array();
-                    photos[i]['img']=this.attributes[1].nodeValue;
+                    photos[i]['img'] = this.attributes[1].nodeValue;
                     i++;
                 }
             );
             getDatas();
         });
-
-        console.log(photos);
-
     }
 );
 
 
-var displaydata = function(){
-    photos.forEach(function(i){
-        var info = "<img src='"+photos[i]['img']+"'/>";
-        $('main').html().append(info);
+var displaydata = function () {
+    photos.forEach(function (i) {
+        var info = "<img src='" + photos[i]['img'] + "'/>";
+        $('#content').html().append(info);
     });
 };
 
-var getDatas = function(){
+var getDatas = function () {
     d3.csv("../php/lecteurFlux.php?url=cdnl1516data",
-           function (data){
-        xmlDoc.forEach(
-            function(d){
-                photos[d.idPhotos]['data']=d;
-            });
-
-    });
+        function (data) {
+            xmlDoc.forEach(
+                function (d) {
+                    if (d.idPhoto != "") {
+                        photos[d.idPhoto]['data'] = d;
+                    }
+                });
+            photos.forEach(
+                function (e) {
+                    var item = "";
+                    item = "<article>";
+                    item += "    <figure>";
+                    item += "        <div style=\"background-image :url('" + e['img'] + "')\" ></div>";
+                    item += "        <figcaption>" + e['data']['Nom'] + " " + e['data']['Prénom'] + "</figcaption>";
+                    item += "    </figure>";
+                    item += "</article>";
+                    html += item;
+                }
+            );
+            $('#content').html(html);
+        });
 
 }
