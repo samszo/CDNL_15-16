bcf216c26784fbce8ca4166082d4764cd35e41bb
Miloud Maamar <CrazyYoshi@MacBook-Pro-de-Miloud.local>
2015-10-18 14:22:00 +0200

    minor update

--- CrazyYoshi/js/main.js
+++ CrazyYoshi/js/main.js
@@ -1,38 +1,50 @@
-	var dataEtu;
-	$(document).ready(
-	    function () {
-	        $.ajax({
-	            type: "GET",
-	            url: "../php/lecteurFlux.php?url=cdnl1516photo",
-	            dataType: "xml",
-	            success: function (xml) {
-	                var i = 0;
-	                $(xml).find('enclosure').each(
-	                    function () {
-	                        //console.log(this.attributes[1].nodeValue);
-	                        $('<div class="items" id="link_' + i + '"></div>').html('<img onclick="voirData(' + i + ')" src="' + this.attributes[1].nodeValue + '" />').appendTo('#Div_XML');
-	                        i++;
-	                    });
-	            }
-	        });
-	        d3.csv("../php/lecteurFlux.php?url=cdnl1516data", function (data) {
-	            dataEtu = data;
-	            /*
-			   data.forEach(function(d,i){
-				   console.log(d.idPhoto+" "+d.Nom);
-			   })
-			   */
-	        });
-	    }
-	);
+var dataEtu;
+var xmlDoc;
+var photos = new Array();
 
-	function voirData(id) {
-	    //chercher les data
-	    dataEtu.forEach(function (d) {
-	        if (d.idPhoto == id && d.idPhoto != "") {
-	            $('<div class="data" id="data_' + id + '"></div>').html(d.Prénom + " " + d.Nom).appendTo('#link_' + id);
+$(document).ready(
+    function () {
+        d3.csv("../php/lecteurFlux.php?url=cdnl1516data", function (data){xmlDoc=data;});
+        $.ajax({
+            type: "GET",
+            url: "../php/lecteurFlux.php?url=cdnl1516photo",
+            dataType: "xml",
+            success: function (xml) {
+                return xml;
+            }
+        }).done(function(data){
+            var i = 0;
+            $(data).find('enclosure').each(
+                function () {
+                    photos[i] = new Array();
+                    photos[i]['img']=this.attributes[1].nodeValue;
+                    i++;
+                }
+            );
+            getDatas();
+        });
 
-	        }
-	    })
+        console.log(photos);
 
-	}
+    }
+);
+
+
+var displaydata = function(){
+    photos.forEach(function(i){
+        var info = "<img src='"+photos[i]['img']+"'/>";
+        $('main').html().append(info);
+    });
+};
+
+var getDatas = function(){
+    d3.csv("../php/lecteurFlux.php?url=cdnl1516data",
+           function (data){
+        xmlDoc.forEach(
+            function(d){
+                photos[d.idPhotos]['data']=d;
+            });
+
+    });
+
+}
