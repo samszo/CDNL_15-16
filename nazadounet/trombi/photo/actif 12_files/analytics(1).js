81d69e0ae233c1af25311b5b9d355b3350d22a1c
nazadounet <senouci.o@gmail.com>
2015-10-29 11:24:29 +0100

    ajout dossier

---
+++ nazadounet/trombi/photo/actif 12_files/analytics(1).js
@@ -0,0 +1,36 @@
+(function() {
+var ozoki_tc = 'PWQgdBFP4uSXsyYQBBWkKUYiDOoksExm';
+var ozoki_os = 's.acexedge.com';
+var ozoki_url = 'https://s.acexedge.com/2/618597/analytics.js?ac=8810988&amp;si=1390740&amp;pc=119367023&amp;pi=292025398&amp;cr=63520269&amp;dm=728x90&amp;ai=2229883&amp;ui=AMsySZaAIy9l1LNM-6AW_qgXE0mQ&amp;cb=3829008549&amp;pp=N4022.1337588.AFFIPERF.FR&amp;dt=6185971435323712070001&amp;ti=2189408060105107202&amp;C1=mmh&amp;C2=100620&amp;C3=199586&amp;C4=991568&amp;C5=1647457&amp;';
+
+var ozoki_ct = {"ac":"8810988", "si":"1390740", "pc":"119367023", "pi":"292025398", "cr":"63520269", "dm":"728x90", "ai":"2229883", "ui":"AMsySZaAIy9l1LNM\u002D6AW_qgXE0mQ", "cb":"3829008549", "pp":"N4022.1337588.AFFIPERF.FR", "dt":"6185971435323712070001", "ti":"2189408060105107202", "C1":"mmh", "C2":"100620", "C3":"199586", "C4":"991568", "C5":"1647457"};
+
+if (typeof ozoki_ct == "undefined" || ozoki_ct == null)  {
+var ozoki_ct = {};
+}
+ozoki_ct.ci = '618597';
+
+var ozoki_opt = {sm:30};
+
+ozoki_opt.mouse=true;
+ozoki_opt.dup=true;
+ozoki_opt.wrip = true;
+
+
+var PAGESPEED_VERSION = "4.12.1";
+
+
+
+
+
+
+
+
+
+
+(function() {
+!function(){!function(){var o="2";if(window.ozoki_data){if(!ozoki_opt.dup)return}else window.ozoki_data=[];var i={};i.ozoki_os=ozoki_os,"undefined"!=typeof ozoki_fl&&(i.ozoki_fl=ozoki_fl),i.ozoki_url=ozoki_url,i.ozoki_ct=ozoki_ct,i.ozoki_tc=ozoki_tc,i.ozoki_opt=ozoki_opt,window.ozoki_data.push(i);var t,d=ozoki_url.substr(0,ozoki_url.indexOf(":"))+"://"+ozoki_os+"/"+o+"/"+PAGESPEED_VERSION+"/",_=!1,k=function(){_||(document&&!t&&(t=document.createElement("script"),t.src=d+"loaded.js"),document.body?(_=!0,document.body.appendChild(t)):window.setTimeout(k,4))};k()}()}();}());
+
+
+}());
+
