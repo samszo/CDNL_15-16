4a90dd906ae902e9eb80c3fd289a981750317c2c
Sissiwiki <row@MacBook-Pro-de-row.local>
2015-11-16 19:59:02 +0100

    correction

--- sissiwiki/trombi-vintage/photo/actif 12_files/ads.js
+++
@@ -1,1 +0,0 @@
-isAdBlockActive=false;
--- sissiwiki/trombi-vintage/photo/actif 12_files/cdx10b.js
+++
@@ -1,8 +0,0 @@
-/* Copyright 2010 Cedexis Inc. */
-
-(function() {
-    var s = new Date();
-    if ('object' === typeof window.radar) {
-        window.radar.stoppedAt = s;
-    }
-})();
